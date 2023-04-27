9a982c5c4378904198de8dbf9cd91b2f6fb66977
sumanth <kmsumanth08@gmail.com>
2023-04-27 12:09:59 +0530

    data ingestion completed

--- src/logger.py
+++ src/logger.py
@@ -2,7 +2,7 @@ import os
 import logging
 from datetime import datetime
 
-LOG_FILE=f'{datetime.now().strftime("%m_%d_%Y_%H_%M_%S")}'
+LOG_FILE=f'{datetime.now().strftime("%m_%d_%Y_%H_%M_%S")}.log'
 logs_path=os.path.join(os.getcwd(),'logs',LOG_FILE)
 os.makedirs(logs_path,exist_ok=True)
 
@@ -12,4 +12,5 @@ logging.basicConfig(
     filename=LOG_FILE_PATH,
     level=logging.INFO,
     format='[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s'
+
 )
--- src/pipeline/model_training.py
+++ src/pipeline/model_training.py
@@ -1,12 +1,12 @@
 import os
 import sys
+from src.logger import logging
+from src.exception import CustomException
 import pandas as pd
 
 from src.components.data_ingestion import DataIngestion
-from src.exception import CustomException
-from src.logger import logging
+
 
 if __name__=='__main__':
     obj=DataIngestion()
-    train_data_path,test_data_paht=obj.initiate_data_ingestion()
-
+    train_data_path,test_data_path=obj.initiate_data_ingestion()
